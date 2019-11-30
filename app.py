5d93f891e47fec301d40a6fbb7dbdb0cb6649a22
kausil24 <kaushil.kundalia@gmai.com>
2019-11-30 13:22:51 +0530

    Flask Application

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, Response, redirect
+import base64
+import jsonpickle
+from datetime import datetime
+from dateExt import dateExtractor
+from textExt import TesseractTxtExtractor
+from preProc import preProcessor
+import pandas as pd 
+import os 
+from PIL import Image
+import cv2 as cv
+
+app = Flask(__name__)
+pp = preProcessor()
+tx = TesseractTxtExtractor()
+dx = dateExtractor()
+
+@app.route("/extract_date", methods=['GET','POST'])
+def index():
+    if request.method == "POST":
+        r = request
+        # if r.method=="POST":
+        image_64_decode = base64.decodestring(r.data)
+        image_result = open('recieved_img.jpeg', 'wb')
+        image_result.write(image_64_decode)
+        mtx = cv.imread('recieved_img.jpeg')
+        content = tx.extractTxt(mtx)
+        date = dx.extractDate(content)
+        response = {'date' : date }
+        # encode response using jsonpickle
+        response_pickled = jsonpickle.encode(response)
+        
+        # print(r.data)
+        
+    elif request.method=="GET":
+        response_pickled = "Make a POST request and see the result on your console"
+
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+        
+    # else:
+    #     return Response(response="No Data", status=200, mimetype="application/json")
+    
+
+if __name__ == "__main__":
+    app.run(debug = True)
